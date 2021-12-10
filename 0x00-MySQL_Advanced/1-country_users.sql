c44aa7eb8e8d5b40f901cfa46984b95cea2814a5
kiminzajnr <kiminzaeric@gmail.com>
2021-12-10 06:59:35 +0000

    enumeration

---
+++ 0x00-MySQL_Advanced/1-country_users.sql
@@ -0,0 +1,8 @@
+-- Creates a table users
+-- Script should be executed on any database
+CREATE TABLE IF NOT EXISTS users(
+	id INT PRIMARY KEY NOT NULL AUTO_INCREMENT UNIQUE,
+	email VARCHAR(255) NOT NULL UNIQUE,
+	name VARCHAR(255),
+	country ENUM('US', 'CO', 'TN') NOT NULL DEFAULT 'US'
+	)
