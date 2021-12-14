8bd68ecf27503ed2a7912f9c686053868fee17c1
Erick Kiminza <kiminzaeric@gmail.com>
2021-12-14 15:20:22 +0300

    change school topics

--- 0x01-NoSQL/10-update_topics.py
+++ 0x01-NoSQL/10-update_topics.py
@@ -6,6 +6,6 @@ Change school topics
 
 def update_topics(mongo_collection, name, topics):
     """changes all school topics based on the name"""
-    if mongo_collection['name'] == name:
-        mongo_collection['name'] = topics
-    return mongo_collection
+    myquery = {"name": name}
+    newvalues = {"$set": {"topics": topics}}
+    mongo_collection.update(myquery, newvalues)
