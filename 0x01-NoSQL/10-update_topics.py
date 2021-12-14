402ecc072f4a78ef9fd0dd79c5cbbc10c943eb84
Erick Kiminza <kiminzaeric@gmail.com>
2021-12-14 15:25:04 +0300

    update all

--- 0x01-NoSQL/10-update_topics.py
+++ 0x01-NoSQL/10-update_topics.py
@@ -8,4 +8,4 @@ def update_topics(mongo_collection, name, topics):
     """changes all school topics based on the name"""
     myquery = {"name": name}
     newvalues = {"$set": {"topics": topics}}
-    mongo_collection.update(myquery, newvalues)
+    mongo_collection.update_many(myquery, newvalues)
